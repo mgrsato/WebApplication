36026be0168b32d2fbaf88c0c48cf905d8ea1ab7
mgrsato <mgrsato@gmail.com>
2019-06-28 16:07:52 +0900

    Update select_weather3.php

--- auto_insert/select_weather3.php
+++ auto_insert/select_weather3.php
@@ -26,34 +26,38 @@
 	$charset = 'utf8';
 	$user = 'dbuser';
 	$password = 'dbuserpass';
+	
 	$dsn = 'mysql:host='.$host.';dbname='.$dbname.';charset='.$charset;
 	$dbh = new PDO($dsn, $user, $password);
 	$sql = 'SELECT * FROM weather3';
 	// $sql = 'SELECT * FROM weather3 order by id DESC limit 10';
 	$stmt = $dbh->query($sql);
+	
 	print("<table>");
 	print("<tr>");
-    print("<th>id</th>");
-    print("<th>obs_time</th>");
-    print("<th>obs_weather</th>");
-    print("<th>obs_temperature</th>");
-    print("<th>obs_humidity</th>");
-    print("<th>obs_pressure</th>");
-    print("<th>obs_wind_direction</th>");
-    print("<th>obs_wind_speed</th>");
+    	print("<th>id</th>");
+    	print("<th>obs_time</th>");
+    	print("<th>obs_weather</th>");
+    	print("<th>obs_temperature</th>");
+    	print("<th>obs_humidity</th>");
+    	print("<th>obs_pressure</th>");
+    	print("<th>obs_wind_direction</th>");
+    	print("<th>obs_wind_speed</th>");
 	print("</tr>");
+	
 	foreach ($stmt as $row) {
 		print("<tr>");
-        print("<td>".$row['id']."</td>");
-        print("<td>".$row['obs_time']."</td>");
-        print("<td>".$row['obs_weather']."</td>");
-        print("<td>".$row['obs_temperature']."</td>");
-        print("<td>".$row['obs_humidity']."</td>");
-        print("<td>".$row['obs_pressure']."</td>");
-        print("<td>".$row['obs_wind_direction']."</td>");
-        print("<td>".$row['obs_wind_speed']."</td>");
+        	print("<td>".$row['id']."</td>");
+        	print("<td>".$row['obs_time']."</td>");
+        	print("<td>".$row['obs_weather']."</td>");
+        	print("<td>".$row['obs_temperature']."</td>");
+        	print("<td>".$row['obs_humidity']."</td>");
+        	print("<td>".$row['obs_pressure']."</td>");
+        	print("<td>".$row['obs_wind_direction']."</td>");
+        	print("<td>".$row['obs_wind_speed']."</td>");
 		print("</tr>");
 	}
+	
 	print("</table>");
 	$dbh = null;
 ?>
