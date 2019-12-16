9189535e5ccabd6044dfba387830f9e1d3b874c7
mgrsato <mgrsato@gmail.com>
2019-12-16 14:57:42 +0900

    Add files via upload

---
+++ yahooAPI/morphological_analysis.php
@@ -0,0 +1,22 @@
+<!-- Morphological analysis -->
+
+<?php
+$appid = '<あなたのアプリケーションID>';
+
+// $str = '今日は良い天気ですね';
+$str = "";
+if(isset($_GET['str'])) {
+    $str = htmlspecialchars($_GET['str']);
+}
+
+$url = 'https://jlp.yahooapis.jp/MAService/V1/parse?appid=' . $appid . '&results=ma,uniq&uniq_filter=9%7C10&sentence=' . urlencode($str);
+$xml = simplexml_load_file($url);
+
+foreach( $xml->ma_result->word_list->word as $list){
+    print '<p>surface : ' . $list->surface . '</p>';
+    print '<p>reading : ' . $list->reading . '</p>';
+    print '<p>pos : ' . $list->pos . '</p>';
+    print '<br>';
+}
+
+?>
