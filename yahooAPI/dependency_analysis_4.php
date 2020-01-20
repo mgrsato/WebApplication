40aca461c1188e99d87a7f17b7cf7c79d4ad78bc
mgrsato <mgrsato@gmail.com>
2020-01-20 15:42:04 +0900

    Add files via upload

--- yahooAPI/dependency_analysis_4.php
+++ yahooAPI/dependency_analysis_4.php
@@ -4,12 +4,11 @@
 	<TITLE>Dependency Analysis</TITLE>
 	<STYLE TYPE="text/css">
 		div {
-			/* width: 400px; */
-			width: 800px;
+			width: 400px;
 			/* height: 400px; */
 			border: solid 1px #708090;
 			padding: 15px;
-			/* margin-bottom: 20px; */
+			/* margin-bottom: 10px; */
 			margin: 10px;
 			border-radius: 4px;
 			float:left;
@@ -21,40 +20,37 @@
 <?php
 	$appid = '<あなたのアプリケーションID>';
 
-	// $str = "";
-	// if(isset($_GET['str'])) {
-	// 	$str = htmlspecialchars($_GET['str']);
-	// }
-
-	// see https://headlines.yahoo.co.jp/rss/list
-	// see https://news.google.com/
-	$url_1 = 'https://news.yahoo.co.jp/pickup/domestic/rss.xml';
+	// ---> https://news.google.com/
 	// $url_1 = 'https://news.google.com/rss?hl=ja&gl=JP&ceid=JP:ja';
+	// ---> https://headlines.yahoo.co.jp/rss/list
+	// $url_1 = 'https://news.yahoo.co.jp/pickup/domestic/rss.xml';
+	$url_1 = 'https://news.yahoo.co.jp/pickup/local/rss.xml';
+
 	$xml_1 = simplexml_load_file($url_1);
 	foreach( $xml_1->channel->item as $list_1){
 		print '<div>';
-		// print 'item : ' . $list_1->title . '<br>';
 		print '<H2>' . $list_1->title . '</H2>';
 		$str = $list_1->title;
 		$url_2 = $list_1->link;
 
-		// $xml_2 = simplexml_load_file($url_2);
-		// var_dump($xml_2);
-
 		$html = file_get_contents($url_2);
-		$dom = new DOMDocument();
-		@$dom->loadHTML($html);
-		$xml_2 = simplexml_import_dom($dom);
-		// var_dump($xml_2);
+		$domDocument = new DOMDocument();
+		$domDocument->loadHTML($html);
+		$xmlString = $domDocument->saveXML();
+		$xmlObject = simplexml_load_string($xmlString);
+		// var_dump($xmlObject);
+		$array = json_decode(json_encode($xmlObject), true);
+		// print $array['head']['title'];
+		// $title = $domDocument->getElementsByTagName('title')->item(0)->nodeValue;
+		$text = $domDocument->getElementById('tpcNews')->nodeValue;
+		print '<p>' . $text . '<A HREF="' . $url_2 . '">[LINK]</A></p>';
 
-		$str_2 = $xml_2->meta->attributes()->content;
-		print 'ニュース本文 : ' . $str_2;
-
-		$url_3 = 'https://jlp.yahooapis.jp/DAService/V1/parse?appid=' . $appid . '&sentence=' . urlencode($str);
+		// $url_3 = 'https://jlp.yahooapis.jp/DAService/V1/parse?appid=' . $appid . '&sentence=' . urlencode($str);
+		$url_3 = 'https://jlp.yahooapis.jp/DAService/V1/parse?appid=' . $appid . '&sentence=' . urlencode($text);
 		$xml_3 = simplexml_load_file($url_3);
 		foreach( $xml_3->Result->ChunkList->Chunk as $list_3){
-			// if(strpos($list_3->MorphemList->Morphem->Feature,'地名行政区分') !== false){
-			if(strpos($list_3->MorphemList->Morphem->Feature,'地名') !== false){
+			if(strpos($list_3->MorphemList->Morphem->Feature,'地名行政区分') !== false){
+			// if(strpos($list_3->MorphemList->Morphem->Feature,'地名') !== false){
 				$surface = $list_3->MorphemList->Morphem->Surface;
 				print '<h3>' . $surface . '</h3>';
 
@@ -64,7 +60,7 @@
 					$latlng = explode(",",  $list_4->Coordinates);
 					$lat = $latlng[1];
 					$lng = $latlng[0];
-					print '<p><img width="300" height="200" src="https://map.yahooapis.jp/map/V1/static?appid=' . $appid . '&lat=' . $lat . '&lon=' . $lng . '&z=12&width=300&height=200&pointer=on"></p>';
+					print '<p><img width="400" height="300" src="https://map.yahooapis.jp/map/V1/static?appid=' . $appid . '&lat=' . $lat . '&lon=' . $lng . '&z=12&width=400&height=300&pointer=on"></p>';
 					print '<br>';
 				}
 			}
--- yahooAPI/morphological_analysis_2.php
+++ yahooAPI/morphological_analysis_2.php
@@ -2,28 +2,17 @@
 <HEAD>
 	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<TITLE>Morphological analysis</TITLE>
-	<STYLE TYPE="text/css">
-		div {
-			width: 160px;
-			height: 80px;
-			border: solid 1px #708090;
-			padding: 15px;
-			margin-bottom: 20px;
-			border-radius: 4px;
-			float:left;
-		}
-	</STYLE>
 </HEAD>
 <BODY>
 
 <?php
 $appid = '<あなたのアプリケーションID>';
 
-// $str = '今日は良い天気ですね';
 $str = "";
 if(isset($_GET['str'])) {
     $str = htmlspecialchars($_GET['str']);
 }
+// $str = '今日は良い天気ですね';
 
 $url = 'https://jlp.yahooapis.jp/MAService/V1/parse?appid=' . $appid . '&results=ma,uniq&uniq_filter=9%7C10&sentence=' . urlencode($str);
 $xml = simplexml_load_file($url);
@@ -40,16 +29,6 @@ foreach( $xml->ma_result->word_list->word as $list){
                 print $list->surface;
                 print '　';
 	}
-}
-
-print '<HR>';
-
-foreach( $xml->ma_result->word_list->word as $list){
-    print '<div>';
-    print 'surface : ' . $list->surface . '<br>';
-    print 'reading : ' . $list->reading . '<br>';
-    print 'pos : ' . $list->pos . '';
-    print '</div>';
 }
 
 ?>
